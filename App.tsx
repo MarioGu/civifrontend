f72983c1e88a98eb4a32d910b56acc1da2833fb6
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-29 17:10:11 -0300

    feat: add list to home

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {store} from './src/store';
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="dark-content" hidden />
+      <StatusBar hidden />
       <SafeAreaView>
         <Home />
       </SafeAreaView>
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {FlatList} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {MessageItem} from '../components/MessageItem';
+import {RootState} from '../store';
+import {addMessages} from '../store/actions/messages.actions';
 
 interface Props {}
 
 export const Home: React.FC<Props> = _ => {
-  return <></>;
+  const dispatch = useDispatch();
+  const {messages} = useSelector((state: RootState) => state.messages);
+
+  const updateMessages = (start: number) => {
+    dispatch(addMessages(start));
+  };
+
+  useEffect(() => {
+    if (messages.length === 0) {
+      updateMessages(0);
+    }
+  }, []);
+
+  return (
+    <FlatList
+      data={messages}
+      keyExtractor={item => item.id.toString()}
+      renderItem={({item}) => <MessageItem message={item} />}
+    />
+  );
 };
