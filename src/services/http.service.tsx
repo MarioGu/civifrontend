346c750871211014e35c5150ad44b5cf6aa3a291
Mario Bonaldi <mario.bonaldi@gmail.com>
2021-09-29 17:08:22 -0300

    feat: add http service

---
+++ src/services/http.service.tsx
@@ -0,0 +1,33 @@
+const baseURL = 'http://localhost:5050';
+
+export async function postToServer(path: string, body: string) {
+  let responseData = null;
+  try {
+    const response = await fetch(
+      // process.env.REACT_APP_URL_BASE  +
+      baseURL + path,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body,
+      },
+    );
+    console.log(response);
+    if (!response.ok) {
+      return Promise.reject(response);
+    } else {
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        responseData = await response.json();
+      } else {
+        responseData = await response.text();
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+  return responseData;
+}
